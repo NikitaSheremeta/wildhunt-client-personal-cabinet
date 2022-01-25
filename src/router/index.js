2b0894dbbcf95204da75e1f6cc78527a206e10a8
Nikita SHeremeta <nsheremeta@systematica-consulting.ru>
2022-01-25 20:37:32 +0300

    Added dynamic page titles

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,25 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
+    meta: {
+      title: 'Вход в аккаунт'
+    },
     component: () => import('../views/Login.vue')
   },
   {
     path: '/signup',
     name: 'Signup',
+    meta: {
+      title: 'Регистрация аккаунта'
+    },
     component: () => import('../views/Signup.vue')
   },
   {
     path: '/reset-password',
     name: 'Reset password',
+    meta: {
+      title: 'Восстановление аккаунта'
+    },
     component: () => import('../views/ResetPassword.vue')
   }
 ];
@@ -21,6 +30,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+    ? to.meta.title
+    : 'Minecraft Wild Hunt - Личный кабинет';
+
+  next();
 });
 
 export default router;
